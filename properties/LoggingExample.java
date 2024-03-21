56401c046e0af34e13c9d2be59d39121e0d6b39a
Christo Thomas <116977884+christosam3343@users.noreply.github.com>
2024-03-20 22:00:24 -0500

    Add files via upload

---
+++ properties/LoggingExample.java
@@ -0,0 +1,20 @@
+package logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class LoggingExample {
+	private static final Logger logger = LogManager.getLogger(LoggingExample.class);
+	public static void main(String[] args) {
+		
+		logger.info("Test Info");
+		logger.trace("Test Trace");
+		logger.warn("Test warn");
+		logger.error("Test Error");
+		logger.fatal("Test Fatal");
+		logger.debug("Test Debug");
+
+	}
+
+}
