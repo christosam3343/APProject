e5ec4907ea8f46904188a7045168e38e54968382
joelmason226 <162531372+joelmason226@users.noreply.github.com>
2024-03-20 22:08:20 -0500

    Add files via upload

---
+++ domain/logins.java
@@ -0,0 +1,40 @@
+package domain;
+
+public class logins {
+	private String userName;
+	private String password;
+	
+	public logins() {
+		userName = "";
+		password = "";
+	}
+	
+	public logins(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "logins [userName=" + userName + ", password=" + password + "]";
+	}
+	
+	
+	
+}
