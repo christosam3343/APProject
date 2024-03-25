bc2fa4984917e6d540ac70209d1885e505b6dead
Christo Thomas <116977884+christosam3343@users.noreply.github.com>
2024-03-25 08:02:14 -0500

    Updated server class - partially working

---
+++ AP Project Server 1/src/driver/driver.java
@@ -0,0 +1,17 @@
+package driver;
+import generalinfo.Customer;
+import generalinfo.Staff;
+import generalinfo.TripOrder;
+import server.customerCrud;
+import server.server;
+import server.staffCrud;
+
+public class driver {
+
+	public static void main(String[] args) {
+	server test = new server();
+	
+
+	}
+
+}
