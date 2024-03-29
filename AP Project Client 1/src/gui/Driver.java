efff69f39361d6820e12b6520f5305e92c17caf5
Kemar Christie <kemarchristie15@gmail.com>
2024-03-29 15:31:24 -0500

    Update Driver.java

--- AP Project Client 1/src/gui/Driver.java
+++ AP Project Client 1/src/gui/Driver.java
@@ -1,11 +1,13 @@
 package gui;
 
-public class Driver {
+// Driver class to run the application
+public class Driver 
+{
+	// Main method to start the application
+	public static void main(String[] args) 
+	{
+x		LoginFrame loginFrame = new LoginFrame(); // Main method to start the application
+		loginFrame.setVisible(true); // Main method to start the application
 
-	public static void main(String[] args) {
-		
-		LoginFrame loginFrame = new LoginFrame();
-		loginFrame.setVisible(true);
 	}
 }
-		
