7ac9e6459f623eff5319ebcd1fc75434ee8078ac
Kemar Christie <kemarchristie15@gmail.com>
2024-03-29 15:48:30 -0500

    Update LoggingExample.java

--- AP Project Client 1/src/gui/LoggingExample.java
+++ AP Project Client 1/src/gui/LoggingExample.java
@@ -3,11 +3,16 @@ package gui;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-public class LoggingExample {
+// LoggingExample class for demonstrating logging
+public class LoggingExample 
+{
+	// Initialize logger
 	private static final Logger logger = LogManager.getLogger(LoggingExample.class);
-	public static void main(String[] args) {
-		
+
+	// Log different levels of messages
+	public static void main(String[] args) 
+	{
+	        // Log different levels of messages
 		logger.info("Test Info");
 		logger.trace("Test Trace");
 		logger.warn("Test warn");
